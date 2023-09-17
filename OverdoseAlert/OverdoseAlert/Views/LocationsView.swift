16269695c5361dbdfde6a44022f99b2bfb295ea0
mog-rgb <134985952+mog-rgb@users.noreply.github.com>
2023-09-17 04:56:03 -0400

    LocationsView.swift
    Update for dropdown lists

--- OverdoseAlert/OverdoseAlert/Views/LocationsView.swift
+++ OverdoseAlert/OverdoseAlert/Views/LocationsView.swift
@@ -14,15 +14,21 @@ struct LocationsView: View {
     
     //@StateObject private var vm = LocationsViewModel()
     @EnvironmentObject private var vm: LocationsViewModel
-    @State private var mapRegion: MKCoordinateRegion = MKCoordinateRegion(
-        center: CLLocationCoordinate2D(latitude: 39.3335, longitude: -76.6192),
-        span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
-    )
+    
     
     var body: some View {
         ZStack {
-            Map(coordinateRegion: $mapRegion)
+            Map(coordinateRegion: $vm.mapRegion)
                 .ignoresSafeArea()
+            
+            
+            VStack(spacing: 0) {
+                header
+                    .padding()
+                
+                Spacer()
+            }
+           
         }
     }
 }
@@ -32,4 +38,46 @@ struct LocationsView_Previews: PreviewProvider {
         LocationsView()
             .environmentObject(LocationsViewModel())
     }
+}
+
+
+extension LocationsView {
+    
+    private var header: some View {
+        
+        VStack{
+            Button(action: vm.toggleLocationsList) {
+                Text(vm.mapLocation.name + ", " + vm.mapLocation.cityName)
+                    .font(.title2)
+                    .fontWeight(.black)
+                    .foregroundColor(.primary)
+                        .frame(height: 55)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.purple)
+                    .overlay(alignment: .leading) {
+                    
+                        Image(systemName: "arrow.down")
+                            .font(.headline)
+                            .foregroundColor(.primary)
+                            .padding()
+                            .rotationEffect(Angle(degrees: vm.showLocationsList ? 180: 0))
+                          
+                    }
+            }
+            
+            
+            if vm.showLocationsList {
+                LocationsListView()
+            }
+            
+           // LocationsListView()
+            
+            
+        }
+        .background(.thickMaterial)
+        .cornerRadius(10)
+        .shadow(color: Color.black.opacity(0.3), radius: 20, x:0, y:15)
+    }
+    
+    
 }
